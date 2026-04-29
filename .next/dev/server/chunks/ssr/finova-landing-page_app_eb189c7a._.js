6f879c59f5023517d0fcbdf4498ef6fe978f4058
damaris <dzambranoz@unemi.edu.ec>
2026-04-29 15:54:07 -0500

    fix: split build and deploy jobs

--- .next/dev/server/chunks/ssr/finova-landing-page_app_eb189c7a._.js
+++ .next/dev/server/chunks/ssr/finova-landing-page_app_eb189c7a._.js
@@ -1,7 +1,7 @@
 module.exports = [
 "[project]/finova-landing-page/app/favicon.ico (static in ecmascript, tag client)", ((__turbopack_context__) => {
 
-__turbopack_context__.v("/_next/static/media/favicon.0b3bf435.ico");}),
+__turbopack_context__.v("/Pag_Finanzas/_next/static/media/favicon.0b3bf435.ico");}),
 "[project]/finova-landing-page/app/favicon.ico.mjs { IMAGE => \"[project]/finova-landing-page/app/favicon.ico (static in ecmascript, tag client)\" } [app-rsc] (structured image object, ecmascript)", ((__turbopack_context__) => {
 "use strict";
 
